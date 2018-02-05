eed6bc9f62cd369055dcf06d1714d45727defac1
realonlytomd <mclaughlintomd@gmail.com>
2018-02-04 22:39:15 -0600

    changed news site to theVerge

--- server.js
+++ server.js
@@ -38,12 +38,12 @@ mongoose.connect("mongodb://localhost/newsReader", {
 // A GET route for scraping the echojs website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://www.theverge.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("h2.c-entry-box--compact__title").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
