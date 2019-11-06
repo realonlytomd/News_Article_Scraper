2acf9470f67339d6a1d296c7536d69b5ee0f08c7
Tom McLaughlin <mclaughlintomd@gmail.com>
2019-11-06 16:17:40 -0600

    Still no perfect, better, need to update articles sep

--- public/app.js
+++ public/app.js
@@ -1,172 +1,176 @@
 $(document).ready(function(){
 
-//var moment = require("moment");
-//Perform the initial scrape, create and fill the database when app is run
-$.ajax({
-  method: "GET",
-  url: "/scrape"
-}).then(function() {
-});
-// create function to display all the data on the page after retrieved from the db
-function displayData() {
-    // Get the articles as a json
-  $.getJSON("/articles", function(scrapeData) {
-    console.log("scrapeData returned from db:", scrapeData);
-    // For each one
-    // Display the information on the page
-    for (i = 0; i < scrapeData.length; i++) {
-      // if a note for a particular article exists, change font color of title to green
-      // and tell user they've previously added a note
-      if (scrapeData[i].note) {  // if a note exists on (this) article, call function that makes
-        // a slight update to the article's title so that it's updatedAt value is changed,
-        console.log("I'm in the - if there is a scrapeData[i].note!: " + scrapeData[i].note);
-        // var now = moment().format();
-        // console.log("now = " + now);
-        console.log("scrapeData[i], i = ", i);
-        console.log("srapeData[i], scrapeData[i].title = ", scrapeData[i].title);
-        console.log("scrapeData[i], scerapeData[i].update = ", scrapeData[i].update);
-        $.ajax({
-          method: "POST",
-          url: "/articles/test/" + scrapeData[i]._id,
-          data: {
-            // change title slightly so the updatedAt is current, and the article won't be deleted
-            _id: scrapeData[i]._id,
-            update: scrapeData[i].update + 1
-          }
-        })
-        .then(function(dataUpdate) {
-          // Log the response
-          console.log("data from updating title on article: ", dataUpdate);
-        });
-
-        //****
-        $("#articles").append("<p style='color:green;' data-id='" + 
-          scrapeData[i]._id + "'>" + 
-          scrapeData[i].title + "  (You've made a Note!)</p><button data-id='" + 
-          scrapeData[i]._id + "' class='deleteArticle'>Delete Article</button><button><a href='" + 
-          scrapeData[i].link + "' target='_blank'>Go To Article</a></button>");
-      } else {
-        $("#articles").append("<p data-id='" + 
-        scrapeData[i]._id + "'>" + 
-        scrapeData[i].title + "</p><button data-id='" + 
-        scrapeData[i]._id + "' class='deleteArticle'>Delete Article</button><button><a href='" + 
-        scrapeData[i].link + "' target='_blank'>Go To Article</a></button>");
-      }
-    }
-  });
-}
-//
-$(document).on("click", "#scrape", function() {
-  $("#articles").empty();
-  displayData();
-});
-
-// When the title of an article (with a p tag) is clicked
-$(document).on("click", "p", function() {
-  // Empty the notes from the note section
-  $("#notes").empty();
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
-  console.log("thisId after clicking 'p': " + thisId);
-  // Now make an ajax call for the Article
+  //Perform the initial scrape, create and fill the database when app is run
   $.ajax({
     method: "GET",
-    url: "/articles/" + thisId
-  })
-    // With that done, add the note information to the page
-    .then(function(dataSaveP) {
-      console.log("dataSaveP from GET /articles+id", dataSaveP);
-      // The title of the article
-      $("#notes").append("<h2>" + dataSaveP.title + "</h2>");
-      // An input to enter a new note title
-      $("#notes").append("<input id='titleinput' name='title' placeholder='Title'>");
-      // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body' placeholder='Contents'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + dataSaveP._id + "' id='saveNote'>Save Note</button>");
-      // Here's a button to delete a note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + dataSaveP._id + "' id='deleteNote'>Delete Note</button>");
-      // experiment
-      // show the modal
-      $("#noteModal").modal("show");
-      // If there's already a note in the article
-      if (dataSaveP.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(dataSaveP.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(dataSaveP.note.body);
+    url: "/scrape"
+  }).then(function() {
+  });
+
+  // create function to display all the data on the page after retrieved from the db
+  function displayData() {
+      // Get the articles as a json
+    $.getJSON("/articles", function(scrapeData) {
+      console.log("scrapeData returned from db:", scrapeData);
+      // For each one
+      // Display the information on the page
+      for (i = 0; i < scrapeData.length; i++) {
+        // if a note for a particular article exists, change font color of title to green
+        // and tell user they've previously added a 
+        // also, change the update by adding 1. This changes the updatedAt so that it won't be
+        // deleted as an old article (over a week).
+        if (scrapeData[i].note) {
+          $.ajax({
+            method: "POST",
+            url: "/articles/test/" + scrapeData[i]._id,
+            data: {
+              _id: scrapeData[i]._id,
+              update: scrapeData[i].update + 1
+            }
+          })
+          .then(function(dataUpdate) {
+            console.log("dataUpdate: ", dataUpdate);
+          });
+          //****
+          $("#articles").append("<p style='color:green;' data-id='" + 
+            scrapeData[i]._id + "'>" + 
+            scrapeData[i].title + "  (You've made a Note!)</p><button data-id='" + 
+            scrapeData[i]._id + "' class='deleteArticle'>Delete Article</button><button><a href='" + 
+            scrapeData[i].link + "' target='_blank'>Go To Article</a></button>");
+        } else {
+          $("#articles").append("<p data-id='" + 
+          scrapeData[i]._id + "'>" + 
+          scrapeData[i].title + "</p><button data-id='" + 
+          scrapeData[i]._id + "' class='deleteArticle'>Delete Article</button><button><a href='" + 
+          scrapeData[i].link + "' target='_blank'>Go To Article</a></button>");
+        }
       }
     });
-});
-
-// When the Save Note button is clicked
-$(document).on("click", "#saveNote", function() {
-  // Get the id associated with the article
-  var thisId = $(this).attr("data-id");
-  console.log("in #saveNote, thisId = ", thisId);
-  // Run a POST request to change the note, using what's entered in the inputs
-  $.ajax({
-    method: "POST",
-    url: "/articles/" + thisId,
-    data: {
-      // Value taken from title input and the textarea
-      title: $("#titleinput").val(),
-      body: $("#bodyinput").val()
-    }
-  })
-  .then(function(dataSaveNoteUpdate) {
-    // Log the response
-    console.log("data from posting a new Note: ", dataSaveNoteUpdate);
-    // Empty the notes section
-    $("#notes").empty();
-    $("#noteModal").modal("hide");
+  }
+  // the list articles button merely gets all the entries from the db
+  $(document).on("click", "#scrape", function() {
     $("#articles").empty();
     displayData();
   });
-  // Also, remove the values entered in the input and textarea for note entry
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
-});
+  // the delete old button does a relist (to update articles with notes), then calls the api
+  // to delete all the old articles at once.
+  $(document).on("click", "#deleteOld", function() {
+    $("#articles").empty();
+    displayData();
+    $.ajax({
+      method: "DELETE",
+      url: "/articles/deleteold"
+    })
+        //repopulate with the current list of articles
+        //without the recently deleted ones.
+      .then(function(dbDateDelete) {
+        console.log("dbDateDelete: ", dbDateDelete);
+        $("#articles").empty();
+        displayData();
+      });
+  });
 
-// When you click the Delete Note button
-$(document).on("click", "#deleteNote", function() {
-  // Grab the id associated with the article
-  var thisId = $(this).attr("data-id");
-  console.log("delete the note at thisId: ", thisId);
-  // Run a DELETE request to delete the note
-  $.ajax({
-    method: "DELETE",
-    url: "/articles/" + thisId
-  })
-    // still need to empty the notes div as before
-    .then(function() {
+  // When the title of an article (with a p tag) is clicked
+  $(document).on("click", "p", function() {
+    // Empty the notes from the note section
+    $("#notes").empty();
+    // Save the id from the p tag
+    var thisId = $(this).attr("data-id");
+    // Nmake an ajax call for the article that the user wants to add a note
+    $.ajax({
+      method: "GET",
+      url: "/articles/" + thisId
+    })
+      // add the note information to the page
+      .then(function(dataSaveP) {
+        // The title of the article
+        $("#notes").append("<h2>" + dataSaveP.title + "</h2>");
+        // An input to enter a new note title
+        $("#notes").append("<input id='titleinput' name='title' placeholder='Title'>");
+        // A textarea to add a new note body
+        $("#notes").append("<textarea id='bodyinput' name='body' placeholder='Contents'></textarea>");
+        // A button to submit a new note, with the id of the article saved to it
+        $("#notes").append("<button data-id='" + dataSaveP._id + "' id='saveNote'>Save Note</button>");
+        // Here's a button to delete a note, with the id of the article saved to it
+        $("#notes").append("<button data-id='" + dataSaveP._id + "' id='deleteNote'>Delete Note</button>");
+        // show the modal
+        $("#noteModal").modal("show");
+        // If there's already a note in the article, show the info.
+        if (dataSaveP.note) {
+          // Place the title of the note in the title input
+          $("#titleinput").val(dataSaveP.note.title);
+          // Place the body of the note in the body textarea
+          $("#bodyinput").val(dataSaveP.note.body);
+        }
+      });
+  });
+
+  // When the Save Note button is clicked
+  $(document).on("click", "#saveNote", function() {
+    // Get the id associated with the article
+    var thisId = $(this).attr("data-id");
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+      method: "POST",
+      url: "/articles/" + thisId,
+      data: {
+        // Value taken from title input and the textarea
+        title: $("#titleinput").val(),
+        body: $("#bodyinput").val()
+      }
+    })
+    .then(function(dataSaveNoteUpdate) {
+      // Log the response
+      // Empty the notes section
       $("#notes").empty();
       $("#noteModal").modal("hide");
       $("#articles").empty();
+      //and relist the articles and show the article with the recently added note as different
       displayData();
     });
+    // Also, remove the values entered in the input and textarea for note entry
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+  });
 
-  // And remove the values entered in the input and textarea as before
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
-});
+  // When you click the Delete Note button
+  $(document).on("click", "#deleteNote", function() {
+    // Grab the id associated with the article
+    var thisId = $(this).attr("data-id");
+    // Run a DELETE request to delete the note
+    $.ajax({
+      method: "DELETE",
+      url: "/articles/" + thisId
+    })
+      // still need to empty the notes div as before
+      .then(function() {
+        $("#notes").empty();
+        $("#noteModal").modal("hide");
+        $("#articles").empty();
+        displayData();
+      });
 
-// When the Delete Article button is clicked
-$(document).on("click", ".deleteArticle", function() {
-  //empty out the articles section in order to repopulate with the current list of articles
-  //after the chosen one is deleted.
-  $("#articles").empty();
-  // Grab the title associated with the article
-  var thisId = $(this).attr("data-id");
-  // Run a DELETE request to delete the article
-  $.ajax({
-    method: "DELETE",
-    url: "/articles/test/" + thisId
-  })
-    .then(function() {
-      //repopulate with the current list of articles
-      //without the recently deleted one.
-     displayData();
-    });
-});
+    // And remove the values entered in the input and textarea as before
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+  });
+
+  // When the Delete Article button is clicked
+  $(document).on("click", ".deleteArticle", function() {
+    //empty out the articles section in order to repopulate with the current list of articles
+    //after the chosen one is deleted.
+    $("#articles").empty();
+    // Grab the title associated with the article
+    var thisId = $(this).attr("data-id");
+    // Run a DELETE request to delete the article
+    $.ajax({
+      method: "DELETE",
+      url: "/articles/test/" + thisId
+    })
+      .then(function() {
+        //repopulate with the current list of articles
+        //without the recently deleted one.
+      displayData();
+      });
+  });
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,15 +1,9 @@
-// new file that contains the api route information.
-// moved from server.js file
-//
 var express = require("express");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var moment = require("moment");
-// var cheerioAdv = require("cheerio-advanced-selectors");
-// cheerio = cheerioAdv.wrap(cheerio);
 var router = express.Router();
 var db = require("../models");
-var imageArray = [];
 // Routes
 module.exports = function(router) {
     // the GET route for scraping The Verge's website
@@ -17,8 +11,6 @@ module.exports = function(router) {
         // First, grab the body of the html of the site with request
         axios.get("https://www.theverge.com/").then(function(response) {
             // Then, load that into cheerio and save it to $ for a shorthand selector
-            // if using cheerio advanced, this should be added back in
-            //cheerio = cheerioAdv.wrap(cheerio);
             var $ = cheerio.load(response.data);
             $("h2.c-entry-box--compact__title").each(function(i, element) {
                 // Save an empty result object outside of functions that assign different
@@ -28,12 +20,12 @@ module.exports = function(router) {
                 .children("a")
                 .text()
                 .trim();
-                //console.log("result.title: " + result.title);
                 result.link = $(this)
                 .children("a")
                 .attr("href");
+                // the update is used to slightly change the article in the db so it's not
+                // deleted as an old article.
                 result.update = 1;
-                //console.log("result.link: " + result.link);
             // Create a new Article in the db using the `result` object built from scraping
             // But only create the new Article in the db if it doesn't already exist
             // This if statement checks to see if the title already is in the db
@@ -42,16 +34,14 @@ module.exports = function(router) {
                     if (prevArticles) {
                         //console.log("This Article already exists: " + prevArticles);
                     } else {
-                        //Below is the original create function - KEEP THIS
                         db.Article.create(result)
                         .then(function(dbArticle) {
                         // View the added result in the console
-                        //console.log("New dbArticle is: " + dbArticle);
                         })
                         .catch(function(err) {
                         // If an error occurred, send it to the client
                         return res.json(err);
-                        }); //KEEP ABOVE
+                        });
                     }
                     })
                     .catch(function(err) {
@@ -66,37 +56,28 @@ module.exports = function(router) {
     
     // Route for getting all of the Articles from the db
     router.get("/articles", function(req, res) {
-        //Here insert a function to delete articles in the db
-        // that are older than 1 week using the moment.js library
-        // This is so the db doesn't continue to grow over time. - done
-        //*
-        // But first, insert function to update articles that have a note so
-        // that the article's updatedAt
-        // value becomes "now" - before this deleteMany takes place.
-        // something like db.Article.findOneAndUpdate({ })
-        // I need to add another "column" to the db to track (and change) the number
-        // of times the article is updated. It's called update
         db.Article.find({})
             .then(function(dbArticle) {
-            // If that worked, send them back to the client
-            //console.log("after relist articles button clicked, (find({})dbArticle): " + dbArticle);
             res.json(dbArticle);
             })
             .catch(function(err) {
             // However, if an error occurred, send it to the client
             res.json(err);
             });
+    });
 
+    // Route for deleting articles over a week old.
+    router.delete("/articles/deleteold", function() {
         var oneWeekPrev = moment().subtract(2, "minutes");
         console.log("oneWeekPrev: ", oneWeekPrev);
         // // delete all articles that were updated in a time before 7 days ago.
-        // // this includes articles that have notes stored if over a week old. 
+        // // this dles not include articles that have notes stored, since they
+        // are updated with every display of data. 
         db.Article.deleteMany({ updatedAt: { $lt: oneWeekPrev } })
             .then(function(dbDateDelete){
             console.log("dbDateDelete: ", dbDateDelete);
+            res.json(dbDateDelete);
         });
-            // It's ok to rescrape after deleted, as those articles (probably) won't
-            // still be on the site, and consequently, won't be scraped again. 
     });
     
     // Route for getting a specific Article by id, and then populate it with it's note
@@ -116,20 +97,22 @@ module.exports = function(router) {
             });
     });
 
-    // Route for getting a specific Article by id, and then update it's title
+    // Route for getting a specific Article by id, and then change it's update parameter
     router.post("/articles/test/:id", function(req, res) {
         // Using the id passed in the id parameter, and make a query that finds the matching one in the db
-        console.log("inside POST /articles/test/id:, req.body: ", req.body);
-        db.Article.findOneAndUpdate({ _id: req.body._id }, { update: req.body.update }, { returnNewDocument: true })
-                .then(function(dbArticle) {
+        db.Article.findOneAndUpdate(
+            { _id: req.body._id },
+            { update: req.body.update },
+            { returnNewDocument: true }
+        )
+            .then(function(dbArticle) {
                 // If successful, find an Article with the given id, send it back to the client
                 res.json(dbArticle);
-                console.log("dbArticle from POST /articles/test/id: ", dbArticle);
-                })
-                .catch(function(err) {
-                // but if an error occurred, send it to the client
+            })
+            .catch(function(err) {
+            // but if an error occurred, send it to the client
                 res.json(err);
-                });
+            });
         });
     
     // Route for saving and/or updating an Article's associated Note
@@ -142,18 +125,19 @@ module.exports = function(router) {
         // Update the Article to be associated with the new Note
         // { new: true } tells the query that we want it to return the updated User,
         // it returns only the original by default
-        //  the Mongoose query returns a promise, we can chain another `.then` 
-        // which receives the result of the query
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+                return db.Article.findOneAndUpdate(
+                    { _id: req.params.id },
+                    { note: dbNote._id },
+                    { new: true }
+                );
             })
             .then(function(dbArticle) {
             // If successful in updating an Article, send it back to the client
-            res.json(dbArticle);
-            console.log("dbArticle from POST /articles/id: ", dbArticle);
+                res.json(dbArticle);
             })
             .catch(function(err) {
             // If an error occurred, send it to the client
-            res.json(err);
+                res.json(err);
             });
     });
     
@@ -164,15 +148,19 @@ module.exports = function(router) {
             .then(function(dbNote) {
             // If a Note was deleted successfully, 
             // the following is much like saving a note
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+                return db.Article.findOneAndUpdate(
+                    { _id: req.params.id }, 
+                    { note: dbNote._id }, 
+                    { new: true }
+                );
             })
             .then(function(dbArticle) {
             // If we were able to successfully update an Article, send it back to the client
-            res.json(dbArticle);
+                res.json(dbArticle);
             })
             .catch(function(err) {
             // If an error occurred, send it to the client
-            res.json(err);
+                res.json(err);
             });
     });
     
@@ -180,14 +168,16 @@ module.exports = function(router) {
     router.delete("/articles/test/:id", function(req, res) {
     // Using the title passed in the title parameter, and make a query that finds the matching one in the db
         console.log("this is the id of the doc I want to delete: " + req.params.id);
-        db.Article.deleteOne({ _id: req.params.id })
-            .then(function(dbArticle) {
+        db.Article.deleteOne(
+            { _id: req.params.id }
+        )
+        .then(function(dbArticle) {
             // If successful, give the list without the given title, send it back to the client 
             res.json(dbArticle);
-            })
-            .catch(function(err) {
+        })
+        .catch(function(err) {
             // but if an error occurred, send it to the client
             res.json(err);
-            });
+        });
     });
 };
